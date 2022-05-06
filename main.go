da4d3b11b194d5f5e6671e91ac82a259e40db7ee
Artyom Nesterenko <artnest789@gmail.com>
2022-05-06 18:18:08 +0300

    Add command handling
    Handle commands sent to the bot.
    Add help text.

--- main.go
+++ main.go
@@ -6,16 +6,22 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+const help = `
+Available commands:
+/add_timezone label timezone — Add new time zone
+/remove_timezone label — Remove time zone with label
+/clear_timezones — Remove all time zones
+/time — - Show time with time zones`
+
 func main() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 
 	bot.Debug = true
 
 	updateConfig := tgbotapi.NewUpdate(0)
-
 	updateConfig.Timeout = 30
 
 	updates := bot.GetUpdatesChan(updateConfig)
@@ -25,11 +31,21 @@ func main() {
 			continue
 		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
+		if !update.Message.IsCommand() {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+		switch update.Message.Command() {
+		case "help":
+			msg.Text = help
+		default:
+			msg.Text = "Unknown command"
+		}
 
 		if _, err := bot.Send(msg); err != nil {
-			panic(err)
+			log.Panic(err)
 		}
 	}
 }
