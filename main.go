98c32c22058be0fe3abc33de49476cbe503cd487
Artyom Nesterenko <artnest789@gmail.com>
2022-05-06 13:55:26 +0300

    Implement echo bot as a scaffold

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
-	"log"
 	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
-	err := os.Mkdir("timezones", 0755)
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
+	}
+
+	bot.Debug = true
+
+	updateConfig := tgbotapi.NewUpdate(0)
+
+	updateConfig.Timeout = 30
+
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		if _, err := bot.Send(msg); err != nil {
+			panic(err)
+		}
 	}
 }
