83e55122ff6b0604978d0267113f758b3a7c3cab
Artyom Nesterenko <artnest789@gmail.com>
2022-11-27 13:23:58 +0200

    Specify not null columns

--- priv/repo/migrations/20221123160428_create_time_zones.exs
+++ priv/repo/migrations/20221123160428_create_time_zones.exs
@@ -3,9 +3,9 @@ defmodule TimeZoneSyncBot.Repo.Migrations.CreateTimeZones do
 
   def change do
     create table(:time_zones) do
-      add :chat_id, :int8
-      add :label, :text
-      add :location, :text
+      add :chat_id, :int8, null: false
+      add :label, :text, null: false
+      add :location, :text, null: false
     end
 
     create unique_index(:time_zones, [:label, :chat_id])
