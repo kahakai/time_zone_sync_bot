386f2d363e27bddfe6ab4ff3fc190b3c4b9e8200
Artyom Nesterenko <code+github@nesterenko.dev>
2022-12-09 23:46:16 +0200

    Add tests for Commands.Error module

---
+++ test/time_zone_sync_bot/commands/error_test.exs
@@ -0,0 +1,24 @@
+defmodule TimeZoneSyncBot.Commands.ErrorTest do
+  use ExUnit.Case, async: true
+
+  test "returns a map of error messages composed in a human friendly way" do
+    expected_error_messages = %{
+      label: ["can't be blank"],
+      location: ["can't be blank"]
+    }
+
+    time_zone = %TimeZoneSyncBot.TimeZone{}
+    params = %{
+      chat_id: 1,
+      label: "",
+      location: ""
+    }
+
+    changeset = TimeZoneSyncBot.TimeZone.changeset(time_zone, params)
+
+    error_messages = TimeZoneSyncBot.Commands.Error.extract_error_messages(changeset)
+    
+    assert error_messages == expected_error_messages
+  end
+end
+
