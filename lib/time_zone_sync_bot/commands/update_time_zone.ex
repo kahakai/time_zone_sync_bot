4526fa381fc03a9fee35568b0bf617d571d17401
Artyom Nesterenko <code+github@nesterenko.dev>
2022-12-11 01:35:47 +0200

    Update Commands.UpdateTimeZone module
    Handle nil case when no time zone returned from the repo.

--- lib/time_zone_sync_bot/commands/update_time_zone.ex
+++ lib/time_zone_sync_bot/commands/update_time_zone.ex
@@ -2,26 +2,37 @@ defmodule TimeZoneSyncBot.Commands.UpdateTimeZone do
   require Ecto.Query
 
   def execute(chat_id, label, new_location) do
-    time_zone = TimeZoneSyncBot.TimeZone
+    query =
+      TimeZoneSyncBot.TimeZone
       |> Ecto.Query.where(chat_id: ^chat_id)
       |> Ecto.Query.where(label: ^label)
-      |> TimeZoneSyncBot.Repo.one
-    params = %{location: new_location}
 
-    changeset = TimeZoneSyncBot.TimeZone.changeset(time_zone, params)
+    case TimeZoneSyncBot.Repo.one(query) do
+      nil ->
+        error_messages = %{
+          label: ["not found"]
+        }
 
-    case TimeZoneSyncBot.Repo.update(changeset) do
-      {:ok, updated_time_zone} ->
-        %TimeZoneSyncBot.TimeZone{
-          label: updated_label,
-          location: updated_location
-        } = updated_time_zone
-
-        {:ok, "#{updated_label} has been updated with #{updated_location}."}
-      {:error, changeset} ->
-        error_messages = TimeZoneSyncBot.Commands.Error.extract_error_messages(changeset)
         {:error, error_messages}
+
+      time_zone ->
+        params = %{location: new_location}
+
+        changeset = TimeZoneSyncBot.TimeZone.changeset(time_zone, params)
+
+        case TimeZoneSyncBot.Repo.update(changeset) do
+          {:ok, updated_time_zone} ->
+            %TimeZoneSyncBot.TimeZone{
+              label: updated_label,
+              location: updated_location
+            } = updated_time_zone
+
+            {:ok, "#{updated_label} has been updated with #{updated_location}."}
+
+          {:error, changeset} ->
+            error_messages = TimeZoneSyncBot.Commands.Error.extract_error_messages(changeset)
+            {:error, error_messages}
+        end
     end
   end
 end
-
