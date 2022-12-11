912b94c87449624a6b04b4dc2d771863f8f7b1cd
Artyom Nesterenko <code+github@nesterenko.dev>
2022-12-11 13:50:53 +0200

    Update Commands.RemoveTimeZone module
    Handle nil case when no time zone returned from the repo.

--- lib/time_zone_sync_bot/commands/remove_time_zone.ex
+++ lib/time_zone_sync_bot/commands/remove_time_zone.ex
@@ -2,23 +2,33 @@ defmodule TimeZoneSyncBot.Commands.RemoveTimeZone do
   require Ecto.Query
 
   def execute(chat_id, label) do
-    time_zone = TimeZoneSyncBot.TimeZone
+    query =
+      TimeZoneSyncBot.TimeZone
       |> Ecto.Query.where(chat_id: ^chat_id)
       |> Ecto.Query.where(label: ^label)
-      |> TimeZoneSyncBot.Repo.one
 
-    case TimeZoneSyncBot.Repo.delete(time_zone) do
-      {:ok, deleted_time_zone} ->
-        %TimeZoneSyncBot.TimeZone{
-          label: deleted_label,
-          location: deleted_location
-        } = deleted_time_zone
+    case TimeZoneSyncBot.Repo.one(query) do
+      nil ->
+        error_messages = %{
+          label: ["not found"]
+        }
 
-        {:ok, "#{deleted_label}: #{deleted_location} has been removed."}
-      {:error, changeset} ->
-        error_messages = TimeZoneSyncBot.Commands.Error.extract_error_messages(changeset)
         {:error, error_messages}
+
+      time_zone ->
+        case TimeZoneSyncBot.Repo.delete(time_zone) do
+          {:ok, deleted_time_zone} ->
+            %TimeZoneSyncBot.TimeZone{
+              label: deleted_label,
+              location: deleted_location
+            } = deleted_time_zone
+
+            {:ok, "#{deleted_label}: #{deleted_location} has been removed."}
+
+          {:error, changeset} ->
+            error_messages = TimeZoneSyncBot.Commands.Error.extract_error_messages(changeset)
+            {:error, error_messages}
+        end
     end
   end
 end
-
