927ddc10741bf18ec5017294b9c1d7b40e8733e7
Artyom Nesterenko <code+github@nesterenko.dev>
2023-01-29 16:59:28 +0200

    Add Application module

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,3 +2,7 @@ import Config
 
 config :elixir, :time_zone_database, Tz.TimeZoneDatabase
 
+config :time_zone_sync_bot,
+  host: "#{System.fetch_env!("APP_NAME")}",
+  token: System.fetch_env!("BOT_TOKEN"),
+  local_port: System.get_env("PORT", "4000") |> String.to_integer()
--- lib/time_zone_sync_bot/application.ex
+++ lib/time_zone_sync_bot/application.ex
@@ -3,9 +3,19 @@ defmodule TimeZoneSyncBot.Application do
 
   @impl true
   def start(_type, _args) do
+    webhook_config = [
+      host: Application.fetch_env!(:time_zone_sync_bot, :host),
+      local_port: Application.fetch_env!(:time_zone_sync_bot, :local_port)
+    ]
+
+    bot_config = [
+      token: Application.fetch_env!(:time_zone_sync_bot, :token)
+    ]
+
     children = [
       TimeZoneSyncBot.Repo,
-      {Tz.UpdatePeriodically, []}
+      {Tz.UpdatePeriodically, []},
+      {Telegram.Webhook, config: webhook_config, bots: [{TimeZoneSyncBot.Bot, bot_config}]}
     ]
 
     opts = [strategy: :one_for_one, name: TimeZoneSyncBot.Supervisor]
