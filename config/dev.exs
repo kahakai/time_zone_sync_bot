18132fea9489591fbdd00a780c63942d07e89fd0
Artyom Nesterenko <code+github@nesterenko.dev>
2023-04-08 23:31:24 +0300

    Add DATABASE_URL env variable to config

--- config/dev.exs
+++
@@ -1,6 +0,0 @@
-import Config
-
-config :time_zone_sync_bot, TimeZoneSyncBot.Repo,
-  database: "time_zone_sync_bot_dev",
-  hostname: "localhost"
-
--- config/runtime.exs
+++ config/runtime.exs
@@ -3,7 +3,10 @@ import Config
 config :elixir, :time_zone_database, Tz.TimeZoneDatabase
 
 config :time_zone_sync_bot,
-  host: "#{System.fetch_env!("APP_NAME")}",
+  host: System.fetch_env!("APP_NAME"),
   token: System.fetch_env!("BOT_TOKEN"),
   max_bot_concurrency: System.get_env("BOT_MAX_CONCURRENCY", "1000") |> String.to_integer(),
   local_port: System.get_env("PORT", "4000") |> String.to_integer()
+
+config :time_zone_sync_bot, TimeZoneSyncBot.Repo,
+  url: System.get_env("DATABASE_URL", "ecto://localhost/time_zone_sync_bot_#{config_env()}")
