feb9d7120dca1727d542fec5ee75f99c1c43d86c
Artyom Nesterenko <code+github@nesterenko.dev>
2023-04-09 22:30:54 +0300

    Add TimeZoneSyncBot.Release.migrate_data/1

--- lib/time_zone_sync_bot/release.ex
+++ lib/time_zone_sync_bot/release.ex
@@ -2,6 +2,45 @@ defmodule TimeZoneSyncBot.Release do
   @app :time_zone_sync_bot
 
   @doc """
+  Print the migration status for configured Repos' migrations.
+  """
+  def migration_status do
+    load_app()
+
+    for repo <- repos(), do: print_migrations_for(repo)
+  end
+
+  defp print_migrations_for(repo) do
+    paths = repo_migrations_path(repo)
+
+    {:ok, repo_status, _} =
+      Ecto.Migrator.with_repo(repo, &Ecto.Migrator.migrations(&1, paths), mode: :temporary)
+
+    IO.puts(
+      """
+      Repo: #{inspect(repo)}
+        Status    Migration ID    Migration Name
+      --------------------------------------------------
+      """ <>
+        Enum.map_join(repo_status, "\n", fn {status, number, description} ->
+          "  #{pad(status, 10)}#{pad(number, 16)}#{description}"
+        end) <> "\n"
+    )
+  end
+
+  defp repo_migrations_path(repo) do
+    config = repo.config()
+    priv = config[:priv] || "priv/#{repo |> Module.split() |> List.last() |> Macro.underscore()}"
+    config |> Keyword.fetch!(:otp_app) |> Application.app_dir() |> Path.join(priv)
+  end
+
+  defp pad(content, pad) do
+    content
+    |> to_string
+    |> String.pad_trailing(pad)
+  end
+
+  @doc """
   Migrate the database. Defaults to migrating to the latest, `[all: true]`
   Also accepts `[step: 1]`, or `[to: 20200118045751]`
   """
@@ -10,6 +49,19 @@ defmodule TimeZoneSyncBot.Release do
 
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, opts))
+    end
+  end
+
+  @doc """
+  Migrate data in the database. Defaults to migrating to the latest, `[all: true]`
+  Also accepts `[step: 1]`, or `[to: 20200118045751]`
+  """
+  def migrate_data(opts \\ [all: true]) do
+    load_app()
+
+    for repo <- repos() do
+      path = Ecto.Migrator.migrations_path(repo, "data_migrations")
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, path, :up, opts))
     end
   end
 
