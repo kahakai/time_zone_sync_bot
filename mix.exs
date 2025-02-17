374b21c3b099c6fbe5b6bf0666b222c193eb77da
kahakai <git@kahakai.me>
2025-02-17 09:33:40 +0000

    build: v1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TimeZoneSyncBot.MixProject do
   def project do
     [
       app: :time_zone_sync_bot,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.18.2",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
